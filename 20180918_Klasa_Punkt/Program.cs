a8102969c86a958722921d3328e745700ebf9ddc
Marcin Cukrowski <marcin.cukrowski@balluff.pl>
2018-09-18 22:16:58 +0200

    Add project files.

---
+++ 20180918_Klasa_Punkt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20180918_Klasa_Punkt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
