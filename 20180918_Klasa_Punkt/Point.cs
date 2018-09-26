ea4749c5f86389a4e3b27a8591682cba3f7c3987
Marcin Cukrowski <marcin.cukrowski@balluff.pl>
2018-09-26 11:23:22 +0200

    dodano operatory + - *

---
+++ 20180918_Klasa_Punkt/Point.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20180918_Klasa_Punkt
+{
+    class Point
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public static Point operator +(Point p1, Point p2)
+        {
+            Point result = new Point(p1.X + p2.X, p1.Y + p2.Y);
+            return result;
+        }
+
+        public static Point operator -(Point p1, Point p2)
+        {
+            Point result = new Point(p1.X - p2.X, p1.Y - p2.Y);
+            return result;
+        }
+
+        public static Point operator *(Point p1, int mnoznik)
+            {
+            Point result = new Point(p1.X * mnoznik, p1.Y * mnoznik);
+            return result;
+            }
+
+        public void MoveLeft(double value)
+        {
+
+            X = X - value; 
+        }
+
+        public void MoveRight(double value)
+        {
+
+            X = X + value;
+        }
+
+        public void MoveUp(double value)
+        {
+
+            Y = Y + value;
+        }
+        public void MoveDown(double value)
+        {
+
+            Y = Y - value;
+        }
+
+
+        public void show()
+        {
+            Console.WriteLine("Wspolrzedne X:{0} Y:{1}", X, Y);
+            Console.ReadKey();
+        }
+        
+      
+    }
+}
--- 20180918_Klasa_Punkt/Program.cs
+++ 20180918_Klasa_Punkt/Program.cs
@@ -10,6 +10,26 @@ namespace _20180918_Klasa_Punkt
     {
         static void Main(string[] args)
         {
+            Point pt1 = new Point(1,5);
+            Point pt2 = new Point(5, -11);
+            Point pt3 = pt1 + pt2;
+            Point pt4 = pt1 - pt2;
+            Point pt5 = pt4 * 5;
+            pt1.show();
+            pt2.show();
+            pt3.show();
+            pt4.show();
+            pt5.show();
+
+            pt1.MoveDown(5.5);
+            pt1.show();
+            pt1.MoveUp(43.2);
+            pt1.show();
+            pt1.MoveLeft(-99.94);
+            pt1.show();
+            pt1.MoveRight(44.32);
+            pt1.show();
+            
         }
     }
 }
